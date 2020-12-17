bfe824d84a2dcff4c942ca55f7614fb12a0cf0c1
Daniel151505 <daniel150520011@gmail.com>
2020-12-17 07:44:57 -0500

    Creación de los metodos del Formulario

---
+++ Models/Formulario.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AplicacionWebBuscar.Models
+{
+    public class Formulario
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage="Por favor, ingrese un Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage="Por favor, ingrese una Foto")]
+        public string Url { get; set; }
+
+        [Required(ErrorMessage="Por favor, ingrese una Descripción")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage="Por favor, ingrese un Precio")]
+        public double Precio { get; set; }
+
+        [Required(ErrorMessage="Por favor, ingrese un número Celular")]
+        public int Celular { get; set; }
+
+        [Required(ErrorMessage="Por favor, ingrese un Lugar")]
+        public string Lugar { get; set; }
+
+        [Required(ErrorMessage="Por favor, ingrese un Nombre de Usuario")]
+        public string NomUsu { get; set; }
+
+        [Required(ErrorMessage="Por favor ingrese una fecha")]
+        public DateTime FecRes { get; set; }
+    }
+}
